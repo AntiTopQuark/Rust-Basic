adf27c0f7949ddec8a2f2e15700422ca69a90ceb
AntiTopQuark <AntiTopQuark1350@outlook.com>
2025-02-16 21:21:14 +0800

    update result

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +39,16 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        fn insert_to_scores(scores : &mut HashMap<String, Team>, team_name : &String, goals_scored : u8, goals_conceded : u8) {
+            let team = scores.entry(team_name.clone()).or_insert(Team {
+                goals_scored: 0,
+                goals_conceded: 0,
+            });
+            team.goals_scored += goals_scored;
+            team.goals_conceded += goals_conceded;
+        }
+        insert_to_scores(&mut scores, &team_1_name, team_1_score, team_2_score);
+        insert_to_scores(&mut scores, &team_2_name, team_2_score, team_1_score);
     }
     scores
 }
